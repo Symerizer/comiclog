758483ef000dd95c99f63b6620bec6c0e3566cdd
simon <contact@symerweb.com>
2016-10-13 07:20:10 -0400

    added auth scaffolding

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,42 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+@extends('layouts.master')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="/css/app.css" type="text/css">
-    </head>
-    <body>
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
-
-            <div class="container fill">
-                <div class="row">
-                    <div id="menu" class="col-md-2">
-                        <h2>ComicLog</h2>
-                        <ul>
-                            <li><a href="#">My characters</a></li>
-                        </ul>
-                    </div>
-                    <div id="content" class="col-md-10">
-                        <div id="search">
-                            <searchbar></searchbar>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-        <script type="application/javascript" src="/js/app.js"></script>
-    </body>
-</html>
+@section('content')
+    <p>Hullo</p>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="/css/app.css" type="text/css">
+    <script>
+        window.Laravel = <?php echo json_encode([
+                'csrfToken' => csrf_token(),
+        ]); ?>
+    </script>
+</head>
+<body>
+
+<div class="container fill">
+    <div class="row">
+        <div id="menu" class="col-md-2">
+            <h2>ComicLog</h2>
+            <ul>
+                <li><a href="#">My characters</a></li>
+            </ul>
+            <div id="user-info">
+                @if (Auth::guest())
+                    <li><a href="{{ url('/login') }}">Login</a></li>
+                    <li><a href="{{ url('/register') }}">Register</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{ url('/logout') }}"
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
+            </div>
+        </div>
+        <div id="content" class="col-md-10">
+            <div id="search">
+                <searchbar></searchbar>
+                @yield('content')
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script type="application/javascript" src="/js/app.js"></script>
+</body>
+</html>
