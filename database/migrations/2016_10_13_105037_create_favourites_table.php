15ac79f4a803ca9e20373da5ca6cca9f1ad7b04f
simon <contact@symerweb.com>
2016-10-13 07:01:43 -0400

    created database migration for favourites

---
+++ database/migrations/2016_10_13_105037_create_favourites_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFavouritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favourites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('char_id');
+            $table->string('char_name');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('favourites', function(Blueprint $table){
+            $table->dropForeign('favourites_user_id_foreign');
+        });
+    }
+}
